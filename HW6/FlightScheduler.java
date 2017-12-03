4c03b08fe1fcc8845fa0fa68c53936f39767ad0e
Jackson Kolb <JacksonKolb94@gmail.com>
2017-12-02 17:01:23 -0700

    Update FlightScheduler.java

--- HW6/FlightScheduler.java
+++ HW6/FlightScheduler.java
@@ -2,68 +2,86 @@ package flightquery;
 
 import java.util.*;
 import java.io.*;
+import java.sql.*;
 
 public class FlightScheduler {
 
-    public static final Scanner USERINPUT = new Scanner(System.in);
-    public static final String REGEXSTR = "^(1\\-)?[0-9]{3}\\-?[0-9]{3}\\-?"
+    private static final Scanner USERINPUT = new Scanner(System.in);
+    static Connection conn = null;
+    static ResultSet rs = null;
+    static Statement statement = null;
+    private static final String REGEXSTR = "^(1\\-)?[0-9]{3}\\-?[0-9]{3}\\-?"
             + "[0-9]{4}$";
+    private static String firstName, lastName, streetAddress, city, state,
+            zipCode, phone, email, passFirst, passLast, passStreetAdress,
+            passCity, passState, passZipCode, passPhone, passEmail, airlineName,
+            airlineCode, airlineCountry, departureDate, arrivalDate,
+            airportName, airportCity, airportState, bookingDate;
 
     public static void main(String[] args) {
+        UI();
+
+    }
+
+    /*private static void openConnection() {
+        try {
+            conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                    username, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    } */
+    private static void UI() {
         try {
             PrintWriter outFile = new PrintWriter("HW6Error.txt");
-            
-
-            //user information
-            String name,
-                   streetAddress,
-                   city,
-                   state,
-                   zipCode,
-                   phone,
-                   email;
-
-            System.out.println("Enter your name: ");
-            if (isAlpha(USERINPUT.nextLine())) {
-                name = USERINPUT.nextLine();
-            } else {
-                outFile.println("user provided name is not a valid name. "
-                        + "a-z, A-Z only.");
-            }
-            System.out.println("Enter your street address (just the street "
-                    + "name and number: ");
+            System.out.println("---------------------------");
+            System.out.println(" Flight Reservation System ");
+            System.out.println("---------------------------");
+            System.out.print("First name: ");
+            firstName = USERINPUT.nextLine();
+            System.out.print("Last name: ");
+            lastName = USERINPUT.nextLine();
+            System.out.print("Street address "
+                    + "(do not include city, state, or zip): ");
             streetAddress = USERINPUT.nextLine();
-            System.out.println("Enter your city: ");
-            if (isAlpha(USERINPUT.nextLine())) {
-                city = USERINPUT.nextLine();
-            } else {
-                outFile.println("user provided city is not a valid city. "
+            System.out.print("City: ");
+            city = USERINPUT.nextLine();
+            System.out.print("State Abbreviation: ");
+            state = USERINPUT.nextLine();
+            System.out.print("5 digit zip code: ");
+            zipCode = USERINPUT.nextLine();
+            System.out.print("Email address:");
+            email = USERINPUT.nextLine();
+
+            if (!(isAlpha(firstName))) {
+                outFile.println(firstName + " is not a valid name. "
                         + "a-z, A-Z only.");
             }
-            System.out.println("Enter your state: ");
-            if (isAlpha(USERINPUT.nextLine())) {
-                state = USERINPUT.nextLine();
-            } else {
-                outFile.println("user provided state is not a valid state. "
+            if (!(isAlpha(lastName))) {
+                outFile.println(lastName + " is not a valid name. "
                         + "a-z, A-Z only.");
             }
-            System.out.println("Enter your phone: ");
-            if (USERINPUT.nextLine().matches(REGEXSTR)) {
-                phone = USERINPUT.nextLine();
-                // if matches 9999999999, 1-999-999-9999 and 999-999-9999
-            } else {
-                outFile.println("user provided phone number is not a valid "
+            if (!(isAlpha(city))) {
+                outFile.println(city + " is not a valid city. a-z, A-Z only.");
+            }
+            if (!(isAlpha(state))) {
+                outFile.println(state + " is not a valid state. "
+                        + "a-z, A-Z only.");
+            }
+            if (!(phone.matches(REGEXSTR))) {
+                outFile.println(phone + " is not a valid "
                         + "phone number. 9999999999, 1-999-999-9999 and "
                         + "999-999-9999 only.");
             }
-            System.out.println("Enter your email: ");
-            if ((USERINPUT.nextLine().contains("@"))) {
-                email = USERINPUT.nextLine();
-            } else {
-                outFile.println("user provided email is not a valid email. "
+            if (!(email.contains("@"))) {
+                outFile.println(email + " is not a valid email. "
                         + "It must contain an @ character.");
             }
+
             outFile.close();
+
         } catch (FileNotFoundException e) {
             System.err.println("File not found");
         }
@@ -72,6 +90,4 @@ public class FlightScheduler {
     public static boolean isAlpha(String name) {
         return name.matches("[a-zA-Z]+");
     }
-    
 }
-
