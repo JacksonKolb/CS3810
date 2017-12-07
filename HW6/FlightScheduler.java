79f5a4df1e622acba0d7985965e7103248c51140
Jackson Kolb <JacksonKolb94@gmail.com>
2017-12-06 17:53:07 -0700

    Update FlightScheduler.java

--- HW6/FlightScheduler.java
+++ HW6/FlightScheduler.java
@@ -13,8 +13,8 @@ public class FlightScheduler {
     private static final String REGEXSTR = "^(1\\-)?[0-9]{3}\\-?[0-9]{3}\\-?"
             + "[0-9]{4}$";
     private static String firstName, lastName, streetAddress, city, state,
-            country, phone, email, passFirst, passLast, passStreetAddress,
-            passCity, passState, passCountry, passPhone, passEmail, airlineName,
+            country, email, passFirstName, passLastName, passStreetAddress,
+            passCity, passState, passCountry, passEmail, airlineName,
             airlineCode, airlineCountry, departureDate, arrivalDate,
             airportName, airportCity, airportState, bookingDate, username, password;
     private static int zipCode, countryCode, localCode, localNumber,
@@ -23,7 +23,7 @@ public class FlightScheduler {
             arrivalHour, arrivalMin, airportCode, bookingCityCode,
             numOfPassengers;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         UI();
         insertPayerData();
         insertPassengerData();
@@ -86,12 +86,6 @@ public class FlightScheduler {
             if (!(isAlpha(state))) {
                 outFile.println(state + " is not a valid state. "
                         + "a-z, A-Z only.");
-                error = true; 
-            }
-            if (!(phone.matches(REGEXSTR))) {
-                outFile.println(phone + " is not a valid "
-                        + "phone number. 9999999999, 1-999-999-9999 and "
-                        + "999-999-9999 only.");
                 error = true;
             }
             if (!(email.contains("@"))) {
@@ -112,9 +106,9 @@ public class FlightScheduler {
             numOfPassengers = Integer.parseInt(USERINPUT.nextLine());
             for (int i = 0; i < numOfPassengers; i++) {
                 System.out.print("Passenger First Name: ");
-                passFirst = USERINPUT.nextLine();
+                passFirstName = USERINPUT.nextLine();
                 System.out.print("Passenger Last Name: ");
-                passLast = USERINPUT.nextLine();
+                passLastName = USERINPUT.nextLine();
                 System.out.print("Passenger Street Address "
                         + "(do not include city, state, or zip): ");
                 passStreetAddress = USERINPUT.nextLine();
@@ -126,8 +120,12 @@ public class FlightScheduler {
                 passZipCode = Integer.parseInt(USERINPUT.nextLine());
                 System.out.print("Passenger Country: ");
                 passCountry = USERINPUT.nextLine();
-                System.out.print("Phone Number: ");
-                passPhone = USERINPUT.nextLine();
+                System.out.println("Country code:");
+                countryCode = Integer.parseInt(USERINPUT.nextLine());
+                System.out.println("Area code:");
+                localCode = Integer.parseInt(USERINPUT.nextLine());
+                System.out.println("Local number:");
+                localNumber = Integer.parseInt(USERINPUT.nextLine());
                 System.out.print("Passenger Email address:");
                 passEmail = USERINPUT.nextLine();
 
@@ -141,6 +139,10 @@ public class FlightScheduler {
                             + "It must have of a length of 5.");
                     error = true;
                 }
+                if (error) {
+                outFile.close();
+                System.exit(0);
+            }
             }
 
         } catch (FileNotFoundException e) {
@@ -188,4 +190,159 @@ public class FlightScheduler {
             System.exit(0);
         }
     }
+
+    private static void insertPassengerData() {
+        try {
+            openConnection();
+            conn.setAutoCommit(false);
+            statement = conn.createStatement();
+            String insertPassenger = "INSERT INTO Passenger (passFirstName, "
+                    + "passLastName, passEmail) +"
+                    + "VALUES ('" + passFirstName + "', '" + passLastName + "', '" + passEmail + ",)";
+            statement.executeUpdate(insertPassenger);
+            String insertPassengerPhone = "INSERT INTO PassengerPhoneNumbers "
+                    + "(countryCode, localCode, localNumber) +"
+                    + "VALUES ('" + countryCode + "', '" + localCode + "', '" + localNumber + ",)";
+            statement.executeUpdate(insertPassengerPhone);
+            String insertPassengerAddress = "INSERT INTO PassengerAddress (passCountry,"
+                    + " passCity, passState, passPostalCode, passStreesAddress) +"
+                    + "VALUES ('" + passCountry + "', '" + passCity + "', '" + passState + ","
+                    + passZipCode + "', '" + passStreetAddress + "',)";
+            statement.executeUpdate(insertPassengerAddress);
+            statement.close();
+            conn.commit();
+            conn.close();
+        } catch (SQLException exception) {
+            System.err.println(exception.getClass().getName() + ": " + exception.getMessage());
+            System.exit(0);
+        }
+    }
+
+    private static void makeReservation() {
+        System.out.print("From what city would your flight originate?: ");
+        String origin = USERINPUT.nextLine();
+        try {
+            openConnection();
+            conn.setAutoCommit(false);
+            statement = conn.createStatement();
+            rs = statement.executeQuery("SELECT * FROM Flights WHERE (originCode = '" + originCode + "')");
+            if (!rs.isBeforeFirst()) {
+                System.out.println("Sorry, that's not a valid origin city, please restart program.");
+                System.exit(0);
+            } else {
+                System.out.println("Here is a list of flights that originate in " + originCode);
+                System.out.println("-------------------------------------------------------------");
+            }
+            while (rs.next()) {
+                String airline = rs.getString("airlineName");
+                int flightNumber = rs.getInt("uniqueFlightNumber");
+                String originCity = rs.getString("originCode");
+                String destinationCity = rs.getString("destinationCode");
+                System.out.println(airline + " flight " + flightNumber + " flies from " + originCity + " to "
+                        + destinationCity);
+            }
+            rs.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        int counter = 0;
+        do {
+            System.out.print("Choose a destination city from the above list: ");
+            String destCode = USERINPUT.nextLine();
+            try {
+                openConnection();
+                conn.setAutoCommit(false);
+                statement = conn.createStatement();
+                rs = statement.executeQuery("SELECT * FROM Flights WHERE (originCode = '" + originCode + "' AND destinationCode = '"
+                        + destCode + "')");
+                if (!rs.isBeforeFirst()) {
+                    System.out.print("Sorry, that's not a valid destination. ");
+                    counter++;
+                    if (counter == 3) {
+                        System.out.println("Maximum attempts reached, goodbye!");
+                        rs.close();
+                        statement.close();
+                        conn.close();
+                        System.exit(0);
+                    }
+                } else {
+                    rs.next();
+                    int flightNumber = rs.getInt("Flight_number");
+                    break;
+                }
+            } catch (SQLException e) {
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+            }
+        } while (counter < 3);
+        try {
+            rs.close();
+            statement.close();
+            conn.close();
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+    }
+    
+     private static void getConfirmation() throws IOException{
+        FileWriter fileWriter = new FileWriter("HW6.flight", true);
+        PrintWriter printWriter = new PrintWriter(fileWriter);
+
+        try {
+            openConnection();
+            conn.setAutoCommit(false);
+            statement = conn.createStatement();
+            rs = statement.executeQuery("SELECT * FROM Flights WHERE uniqueFlightNumber = "+uniqueFlightNumber+"");
+            rs.next();
+            String airline = rs.getString("airlineName");
+            String origin = rs.getString("originCode");
+            String destination = rs.getString("destinationCode");
+            String departDate = rs.getString("departureDate");
+            String departHour = rs.getString("departureHour");
+            String departMin = rs.getString("departureMin");
+            String arrivalHour = rs.getString("arrivalHour");
+            String arrivalMin = rs.getString("arrivalMin");
+            String flightLength = rs.getString("length");
+            String confirmation = "";
+            confirmation += "Customer Information \n";
+            confirmation += "--------------------------\n";
+            confirmation += "Name: "+ firstName + " " + lastName + "\n";
+            confirmation += "Address: " + streetAddress + ", " + city + ", " + state + ", " +
+                    zipCode + "\n";
+            confirmation += "Phone: " + countryCode + localCode + localNumber + "\n";
+            confirmation += "Email: " + email + "\n\n";
+            confirmation += "Flight Information \n";
+            confirmation += "--------------------------\n";
+            confirmation += "Airline: " + airlineName + "\n";
+            confirmation += "Flight number: " + uniqueFlightNumber + "\n";
+            confirmation += "Departing from: " + originCode + "\n";
+            confirmation += "Destination: " + destinationCode + "\n";
+            confirmation += "Date of Departure: " + departDate + "\n";
+            confirmation += "Time of Departure: " + departHour + departMin + "\n";
+            confirmation += "Arrival Time: " + arrivalHour + arrivalMin + "\n";
+            confirmation += "Length of Flight: " + length;
+            printWriter.println(confirmation);
+            String insertBooking = "INSERT INTO Booking (bookingNumber, payerEmail,"
+                    + " payerFirstName, payerLastName, bookingCityCode, bookingDate, numberOfPassengers) VALUES('"+bookingNumber+ " " +firstName+"', "
+                    +lastName+", '"+bookingCityCode+"', '"+bookingDate+"', '"+numOfPassengers+"')";
+            statement.executeUpdate(insertBooking);
+            rs.close();
+            statement.close();
+            printWriter.close();
+            conn.commit();
+            conn.close();
+            System.out.println("Congratulations! Below is your confirmation.");
+            System.out.println();
+            System.out.println(confirmation);
+            System.out.println();
+            System.out.println("Your confirmation has also been saved to the HW6.flight file");
+        } catch (SQLException e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+}
 }
