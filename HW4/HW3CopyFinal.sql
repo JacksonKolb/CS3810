1a1d0d1a6754bb6a9fc9f58d73b37d5c0db43f10
Jackson Kolb <JacksonKolb94@gmail.com>
2017-12-10 16:37:28 -0700

    Create HW3CopyFinal.sql

---
+++ HW4/HW3CopyFinal.sql
@@ -0,0 +1,66 @@
+
+Skip to content
+This repository
+
+    Pull requests
+    Issues
+    Marketplace
+    Explore
+
+    @JacksonKolb94
+
+0
+0
+
+    1
+
+MaxMorrison6/CS3810-Morrison
+Code
+Issues 0
+Pull requests 0
+Projects 0
+Wiki
+Insights
+CS3810-Morrison/HW4Final/HW3CopyFinal.sql
+a249e7c 12 days ago
+@MaxMorrison6 MaxMorrison6 Add files via upload
+24 lines (15 sloc) 950 Bytes
+/*
+CS 3810 - Database
+Jackson Kolb and Max Morrison
+HW3 V3 - Final
+Help from Emily Lupini with keys and contratiants
+*/
+
+\COPY month_to_quarter FROM 'C:\School\Semester4Fall2017\CS3810\Lupini\month_to_quarter.csv' CSV HEADER;
+
+\COPY state_abr FROM 'C:\School\Semester4Fall2017\CS3810\Lupini\state_abr.csv' CSV HEADER;
+
+\COPY location FROM 'C:\School\Semester4Fall2017\CS3810\Lupini\location.csv' CSV HEADER;
+
+\COPY airport FROM 'C:\School\Semester4Fall2017\CS3810\Lupini\airport.csv' CSV HEADER;
+
+\COPY carrier FROM 'C:\School\Semester4Fall2017\CS3810\Lupini\carrier.csv' CSV HEADER;
+
+\COPY flights FROM 'C:\School\Semester4Fall2017\CS3810\Lupini\flights.csv' CSV HEADER;
+
+\COPY aircraft FROM 'C:\School\Semester4Fall2017\CS3810\Lupini\caircraft.csv' CSV HEADER;
+
+\COPY carrier_date FROM 'C:\School\Semester4Fall2017\CS3810\Lupini\carrier_date.csv' CSV HEADER;
+
+\COPY distance FROM 'C:\School\Semester4Fall2017\CS3810\Lupini\distance.csv' CSV HEADER;
+
+    © 2017 GitHub, Inc.
+    Terms
+    Privacy
+    Security
+    Status
+    Help
+
+    Contact GitHub
+    API
+    Training
+    Shop
+    Blog
+    About
+
